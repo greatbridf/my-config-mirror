d6599e0966a0c3e9f5baf263fa3cb3ac0c1b0c67
greatbridf <guobingfang20040616@gmail.com>
2020-08-07 22:55:13 +0800

    update backup script

--- full-backup/incremental_backup.sh
+++ full-backup/incremental_backup.sh
@@ -1,13 +1,18 @@
+if [ ! $BACKUP_FILES ]; then
+    read -p "Please enter the files to backup " BACKUP_FILES
+fi
+
+EXCLUDE_FILE=
+if [ -f exclude-file ]; then
+    EXCLUDE_FILE="-X exclude-file"
+fi
+
 if [ ! -f backup_*_full.tar.gz ]; then
-    tar -g tar_snapshot -czpvf backup_$(date +%F)_full.tar.gz $BACKUP_FILES
+    tar -g tar_snapshot $EXCLUDE_FILE -czpvf backup_$(date +%F)_full.tar.gz -C $BACKUP_FILES .
     exit
 fi
 
 read -p "Please type in the backup's id " NUM
 
-if [ ! $BACKUP_FILES ]; then
-    read -p "Please enter the files to backup " BACKUP_FILES
-fi
-
 cp tar_snapshot tar_snapshot.bak
-tar -g tar_snapshot -czpvf backup_$(date +%F)_incremental_$NUM.tar.gz $BACKUP_FILES
+tar -g tar_snapshot $EXCLUDE_FILE -czpvf backup_$(date +%F)_incremental_$NUM.tar.gz -C $BACKUP_FILES .
