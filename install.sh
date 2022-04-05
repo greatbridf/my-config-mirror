e946c993f320623552aac42801c991a834ca6e08
greatbridf <greatbridf@icloud.com>
2022-04-05 11:09:21 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,93 +1,65 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $__PREFIX ]; then
-  __HOME=$__PREFIX
+if [ "$PREFIX" = "" ]; then
+  PREFIX="$HOME"
 fi
-if [ ! $__HOME ]; then
-    __HOME=$HOME
-fi
-echo "deployment target 'HOME=$__HOME'"
-__DIRS=(
-       "$__HOME/.vim"
-       "$__HOME/.vim/bundle/vundle"
-       "$__HOME/.oh-my-zsh"
-       )
-__FILES=(
-        "$__HOME/.bash_profile"
-        "$__HOME/.gitconfig"
-        "$__HOME/.gitmessage"
-        "$__HOME/.vimrc"
-        "$__HOME/.vim/vundlerc"
-        "$__HOME/.zshrc"
-        "$__HOME/.xinitrc"
-        "$__HOME/.config/i3/config"
-        )
+
+echo "deployment target 'PREFIX=$PREFIX'"
 
 check_file() {
-  if [ -f $1 ]; then
+  if [ -f "$1" ]; then
     echo "test file $1 failed, exiting..."
     exit 1
   fi
 }
 
 check_dir() {
-  if [ -d $1 ]; then
+  if [ -d "$1" ]; then
       echo "test dir $1 failed, exiting..."
       exit 1
   fi
 }
 
-check_file_existance() {
-    for dir in "${__DIRS[@]}"; do
-      check_dir $dir
-    done
-
-    for file in "${__FILES[@]}"; do
-      check_file $file
-    done
-}
-
 create_dir_if_not_exist() {
-    if [ ! -d $1 ]; then
+    if ! [ -d "$1" ]; then
         echo "directory $1 do not exist, creating..."
-        if ! mkdir -p $1; then
+        if ! mkdir -p "$1"; then
             echo "cannot create directory: $1 , exiting..."
             exit 1
         fi
     fi
 }
 
-message() {
-    if [ $2 ]; then
-        echo "deploying $1 to $2"
-    else
+deploy_message() {
+    if [ "$2" = "" ]; then
         echo "deploying $1"
+    else
+        echo "deploying $1 to $2"
     fi
 }
 deploy() {
-    message $1 $2
-    if ! ln -s $(pwd)/$1 $2; then
+    deploy_message "$1" "$2"
+    if ! ln -s "$(pwd)/$1" "$2"; then
         echo "cannot deploy $1 to $2 , exiting..."
         exit 1
     fi
 }
 deploy_to_home() {
-    if [ $2 ]; then
-        deploy $1 $__HOME/$2
+    if [ "$2" = "" ]; then
+        deploy "$1" "$PREFIX/.$1"
     else
-        deploy $1 $__HOME/.$1
+        deploy "$1" "$PREFIX/$2"
     fi
 }
 confirm() {
-    read -p "$1 (y/N) " __FLAG
-    if [ $__FLAG != "y" ]; then
+    read -r -p "$1 (y/N) " flag
+    if ! [ "$flag" = "y" ]; then
         exit 2
     fi
 }
 install() {
     confirm "start deployment?"
 
-    deploy_to_home bash_profile
     deploy_to_home gitconfig
     deploy_to_home gitmessage
 
@@ -100,30 +72,27 @@ install() {
     echo 'oh-my-zsh is recommended. To install, run ./install.sh oh-my-zsh'
 
     echo "[recommended] install package: xorg i3 pulseaudio fcitx feh termite"
-    deploy_to_home xinitrc
-
-    install_i3_config
 
     echo "fin"
     exit
 }
 
 install_i3_config() {
-    create_dir_if_not_exist $HOME/.config/i3
-    deploy i3-config $HOME/.config/i3/config
+    create_dir_if_not_exist "$PREFIX/.config/i3"
+    deploy i3-config "$PREFIX/.config/i3/config"
 }
 
 install_vundle() {
     echo "installing vundle"
-    git clone https://github.com/VundleVim/Vundle.vim.git $__HOME/.vim/bundle/vundle
-    deploy vundle.vimrc $__HOME/.vim/vundlerc
+    git clone https://github.com/VundleVim/Vundle.vim.git "$PREFIX/.vim/bundle/vundle"
+    deploy vundle.vimrc "$PREFIX/.vim/vundlerc"
 }
 
 __dp_rime () {
     if [ "$(uname -s)" = "Linux" ]; then
-        deploy rime/$1 $HOME/.local/share/fcitx5/rime/$1
+        deploy "rime/$1" "$PREFIX/.local/share/fcitx5/rime/$1"
     elif [ "$(uname -s)" = "Darwin" ]; then
-        deploy rime/$1 $HOME/Library/Rime/$1
+        deploy "rime/$1" "$PREFIX/Library/Rime/$1"
     fi
 }
 
@@ -136,7 +105,7 @@ install_rime() {
     __dp_rime wubi86_jidian.schema.yaml
     __dp_rime wubi86_jidian.txt
     echo "installing fcitx5 themes"
-    cd $HOME/.local/share/fcitx5/themes
+    cd "$PREFIX/.local/share/fcitx5/themes"
     git clone https://github.com/sxqsfun/fcitx5-sogou-themes.git
     mv fcitx5-sogou-themes/Alpha-white .
 }
@@ -144,6 +113,27 @@ install_rime() {
 install_oh_my_zsh() {
     echo "installing oh-my-zsh over internet"
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+show_help() {
+cat 1>&2 <<EOF
+Usage: sh install.sh [target]
+    OR sh install.sh all
+    OR sh install.sh i3-config
+    OR sh install.sh vundle
+    OR sh install.sh rime
+    OR sh install.sh oh-my-zsh
+    OR sh install.sh alacritty
+    OR sh install.sh help
+
+    sh install [target]
+
+        install [target] to \$PREFIX/.[target]
+
+    sh install all
+
+        install gitconfig gitmessage vimrc vundle and zshrc
+EOF
 }
 
 case "$1" in
@@ -164,20 +154,27 @@ case "$1" in
         exit
         ;;
     alacritty)
-        _DEPLOY_TARGET=$__HOME/.config/alacritty/alacritty.yml
-        check_file $_DEPLOY_TARGET
-        create_dir_if_not_exist $HOME/.config/alacritty
-        deploy alacritty $_DEPLOY_TARGET
+        _DEPLOY_TARGET="$PREFIX/.config/alacritty/alacritty.yml"
+        check_file "$_DEPLOY_TARGET"
+        create_dir_if_not_exist "$PREFIX/.config/alacritty"
+        deploy alacritty "$_DEPLOY_TARGET"
         exit
         ;;
     '')
-        check_file_existance
+        show_help
+        exit
+        ;;
+    help)
+        show_help
+        exit
+        ;;
+    all)
         install
         exit
         ;;
     *)
-        check_file $__HOME/.$1
-        deploy_to_home $1
+        check_file "$PREFIX/.$1"
+        deploy_to_home "$1"
         echo "fin"
         ;;
 esac
