04b372d88d83a1936ba9951928f1911e9ccf990d
greatbridf <greatbridf@icloud.com>
2026-07-16 01:39:57 +0800

    fish: set tsinghua mirror for rustup
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- config.fish
+++ config.fish
@@ -111,3 +111,6 @@ if status is-interactive
     bind \ej down-or-search
     bind \ek up-or-search
 end
+
+set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
