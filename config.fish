881d6ff81a143742fd741f4925a5fc313a347806
greatbridf <greatbridf@icloud.com>
2026-07-16 01:39:23 +0800

    fish: set homebrew mirror in the new way
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- config.fish
+++ config.fish
@@ -92,10 +92,8 @@ if status is-interactive
         eval (/opt/homebrew/bin/brew shellenv)
     end
 
-    export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
-    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
-    export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
     export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+    export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
     export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
 
     # find
